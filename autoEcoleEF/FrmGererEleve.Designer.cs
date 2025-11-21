4ca5109c3f811fc137923156be6a77aaa6909543
Rahim <rahim.bellahcene@gmail.com>
2025-11-21 17:12:55 +0100

    Ajoutez des fichiers projet.

---
+++ autoEcoleEF/FrmGererEleve.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace autoEcoleEF
+{
+    partial class FrmGererEleve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtAdresse = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtInscription = new System.Windows.Forms.DateTimePicker();
+            this.cmbCredit = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(259, 65);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 26);
+            this.txtNum.TabIndex = 0;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(259, 124);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 26);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(259, 189);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 26);
+            this.txtPrenom.TabIndex = 2;
+            // 
+            // txtAdresse
+            // 
+            this.txtAdresse.Location = new System.Drawing.Point(259, 249);
+            this.txtAdresse.Name = "txtAdresse";
+            this.txtAdresse.Size = new System.Drawing.Size(100, 26);
+            this.txtAdresse.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numero";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Prenom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Adresse";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(81, 318);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Date d\'inscription";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 386);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Crédit horaire";
+            // 
+            // dtInscription
+            // 
+            this.dtInscription.Location = new System.Drawing.Point(259, 313);
+            this.dtInscription.Name = "dtInscription";
+            this.dtInscription.Size = new System.Drawing.Size(246, 26);
+            this.dtInscription.TabIndex = 10;
+            // 
+            // cmbCredit
+            // 
+            this.cmbCredit.FormattingEnabled = true;
+            this.cmbCredit.Location = new System.Drawing.Point(259, 378);
+            this.cmbCredit.Name = "cmbCredit";
+            this.cmbCredit.Size = new System.Drawing.Size(121, 28);
+            this.cmbCredit.TabIndex = 11;
+           
+            // 
+            // FrmGererEleve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbCredit);
+            this.Controls.Add(this.dtInscription);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdresse);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtNum);
+            this.Name = "FrmGererEleve";
+            this.Text = "Gerer des eleves";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtAdresse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtInscription;
+        private System.Windows.Forms.ComboBox cmbCredit;
+    }
+}
---
+++ autoEcoleEF/FrmMenu.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace autoEcoleEF
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eleveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vehiculeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gererToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.leconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajouterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.validerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gererToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.bdgSourceEleve = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bdgSourceEleve)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem,
+            this.eleveToolStripMenuItem,
+            this.vehiculeToolStripMenuItem,
+            this.leconToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitterToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(169, 34);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            // 
+            // eleveToolStripMenuItem
+            // 
+            this.eleveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gererToolStripMenuItem1});
+            this.eleveToolStripMenuItem.Name = "eleveToolStripMenuItem";
+            this.eleveToolStripMenuItem.Size = new System.Drawing.Size(68, 29);
+            this.eleveToolStripMenuItem.Text = "Eleve";
+            this.eleveToolStripMenuItem.Click += new System.EventHandler(this.eleveToolStripMenuItem_Click);
+            // 
+            // vehiculeToolStripMenuItem
+            // 
+            this.vehiculeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gererToolStripMenuItem,
+            this.listeToolStripMenuItem});
+            this.vehiculeToolStripMenuItem.Name = "vehiculeToolStripMenuItem";
+            this.vehiculeToolStripMenuItem.Size = new System.Drawing.Size(97, 29);
+            this.vehiculeToolStripMenuItem.Text = "Vehicule ";
+            // 
+            // gererToolStripMenuItem
+            // 
+            this.gererToolStripMenuItem.Name = "gererToolStripMenuItem";
+            this.gererToolStripMenuItem.Size = new System.Drawing.Size(156, 34);
+            this.gererToolStripMenuItem.Text = "Gerer";
+            // 
+            // listeToolStripMenuItem
+            // 
+            this.listeToolStripMenuItem.Name = "listeToolStripMenuItem";
+            this.listeToolStripMenuItem.Size = new System.Drawing.Size(156, 34);
+            this.listeToolStripMenuItem.Text = "Liste";
+            // 
+            // leconToolStripMenuItem
+            // 
+            this.leconToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajouterToolStripMenuItem,
+            this.validerToolStripMenuItem});
+            this.leconToolStripMenuItem.Name = "leconToolStripMenuItem";
+            this.leconToolStripMenuItem.Size = new System.Drawing.Size(74, 29);
+            this.leconToolStripMenuItem.Text = "Lecon";
+            // 
+            // ajouterToolStripMenuItem
+            // 
+            this.ajouterToolStripMenuItem.Name = "ajouterToolStripMenuItem";
+            this.ajouterToolStripMenuItem.Size = new System.Drawing.Size(172, 34);
+            this.ajouterToolStripMenuItem.Text = "Ajouter";
+            // 
+            // validerToolStripMenuItem
+            // 
+            this.validerToolStripMenuItem.Name = "validerToolStripMenuItem";
+            this.validerToolStripMenuItem.Size = new System.Drawing.Size(172, 34);
+            this.validerToolStripMenuItem.Text = "Valider";
+            // 
+            // gererToolStripMenuItem1
+            // 
+            this.gererToolStripMenuItem1.Name = "gererToolStripMenuItem1";
+            this.gererToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.gererToolStripMenuItem1.Text = "Gerer";
+            this.gererToolStripMenuItem1.Click += new System.EventHandler(this.gererToolStripMenuItem1_Click);
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMenu";
+            this.Text = "Gestion des lecon de conduite";
+            this.Load += new System.EventHandler(this.FrmMenu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bdgSourceEleve)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eleveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vehiculeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem leconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gererToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajouterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem validerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gererToolStripMenuItem1;
+        private System.Windows.Forms.BindingSource bdgSourceEleve;
+    }
+}
+
